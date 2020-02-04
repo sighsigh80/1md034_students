d667dc946da9c6d90743c34ac4e96c93ff7700ac
sighsigh80 <linus735@gmail.com>
2020-02-04 17:52:01 +0100

    Task 4-4 completed

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -5,16 +5,16 @@ function MenuItem(name, pp, cal, alg, veg) {
     this.alergy = alg;
     this.vegan = veg;
     this.info = function() {
-	return this.name + ' ' + 'Calories: ' + this.calories;
+	return this.name + ', calories: ' + this.calories;
     };
 }
 
-let BI1 = new MenuItem('Classic Cheese', 'https://tasteandsee.com/wp-content/uploads/2017/06/Easy-Pimento-Cheese-and-Bacon-Burger-EL-burger-great.jpg', 760, 'lactose', True);
+let BI1 = new MenuItem('Classic Cheese', 'https://tasteandsee.com/wp-content/uploads/2017/06/Easy-Pimento-Cheese-and-Bacon-Burger-EL-burger-great.jpg', 760, 'lactose', 'yes');
 
-let BI2 = new MenuItem('The Tall One', 'https://www.halsanskok.se//sites/default/files/2019-06/190227_GG_Care2share_Incredible%20Burger.jpg', 2000, 'everything', False);
+let BI2 = new MenuItem('The Tall One', 'https://www.halsanskok.se//sites/default/files/2019-06/190227_GG_Care2share_Incredible%20Burger.jpg', 2000, 'everything', 'no');
 
-let BI3 = new MenuItem('Beyond burger', 'https://www.vegomagasinet.se/wp-content/uploads/2018/06/beyond.jpg', 820, 'lactose', True);
+let BI3 = new MenuItem('Beyond burger', 'https://www.vegomagasinet.se/wp-content/uploads/2018/06/beyond.jpg', 820, 'lactose', 'yes');
 
-let BI4 = new MenuItem('Small n Tasty', 'https://cdn-rdb.arla.com/Files/arla-se/1361781217/43b7c050-b61b-4baa-8eb6-5a8ebb896bc4.jpg?crop=(104,0,-152,0)&w=479&h=335&ak=f525e733&hm=72b785fb', 450, 'lactose', True);
+let BI4 = new MenuItem('Small n Tasty', 'https://cdn-rdb.arla.com/Files/arla-se/1361781217/43b7c050-b61b-4baa-8eb6-5a8ebb896bc4.jpg?crop=(104,0,-152,0)&w=479&h=335&ak=f525e733&hm=72b785fb', 450, 'lactose', 'yes');
 
-let BI5 = new MenuItem('Chicken Dream', 'https://barrels.se/app/uploads/2020/01/Unknown.jpeg', 500, 'lactose', False);
+let BI5 = new MenuItem('Chicken Dream', 'https://barrels.se/app/uploads/2020/01/Unknown.jpeg', 500, 'lactose', 'no');
